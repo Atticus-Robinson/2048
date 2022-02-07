4a4d6e68fa1dcb3717d8eb5ae42af0a0bdde8387
Atticus-Robinson <acerobinson2@gmail.com>
2022-02-07 13:41:52 -0600

    updated box colors

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,14 @@
 //ANIMATION TESTING
-$('#test-animation').on('click', function() {
-    $('.number-tile').addClass('addedClass')
-    console.log('test');
-})
+import 'animate.css';
+
+$(function () {
+  $("#test-animation")
+    .on("click", function () {
+      $(".number-tile").addClass("animate");
+      $(".number-tile").removeClass("animate");
+    })
+    .on("animationend", function () {
+      console.log("test");
+      $(".number-tile").removeClass("animate");
+    });
+});
